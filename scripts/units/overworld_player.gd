85d316b6bda9c44eaa63cb9dce226433b373742e
GreggMays <108717496+GreggMays@users.noreply.github.com>
2026-07-08 19:39:37 +0800

    Added dash functionality to both player and enemy

--- scripts/units/overworld_player.gd
+++ scripts/units/overworld_player.gd
@@ -2,10 +2,24 @@ extends CharacterBody2D
 @export var max_speed := 300.0
 @export var acceleration := 900.0
 @export var friction := 1000.0
+@export var after_image_scene: PackedScene
 
 var last_direction := Vector2.DOWN
 var controls_locked: bool = false
 var frontlayer: TileMapLayer
+
+#Dash stuff:
+@export var dash_speed := 700.0
+@export var dash_duration := 0.18
+@export var dash_cooldown := 0.45
+
+const AFTERIMG_INTERVAL := 0.03
+
+var is_dashing := false
+var dash_direction := Vector2.ZERO
+var dash_timer := 0.0
+var dash_duration_timer := 0.0
+var dash_cooldown_timer := 0.0
 
 func _ready() -> void:
 	add_to_group("player")
@@ -25,7 +39,13 @@ func _physics_process(delta: float) -> void:
 		#locks player movement when Scenes load some stuff
 		velocity = Vector2.ZERO
 		return
-	
+		
+	if dash_cooldown_timer > 0.0:
+		dash_cooldown_timer -= delta
+
+	if Input.is_action_just_pressed("dash"):
+		start_dash()
+
 	var input_dir := Input.get_vector(
 		"left",
 		"right",
@@ -34,23 +54,43 @@ func _physics_process(delta: float) -> void:
 	)
 
 	var desired_velocity := Vector2.ZERO
-	if input_dir != Vector2.ZERO:
-		last_direction = input_dir.normalized()
-		desired_velocity = input_dir.normalized() * max_speed
-	else:
-		# Stop immediately when no input (no sliding)
-		desired_velocity = Vector2.ZERO
 
-	var next_global_position: Vector2 = global_position + desired_velocity * delta
-	var next_tile: Vector2i = frontlayer.local_to_map(frontlayer.to_local(next_global_position))
+	if is_dashing:
+		desired_velocity = dash_direction * dash_speed
+	else:
+		if input_dir != Vector2.ZERO:
+			last_direction = input_dir.normalized()
+			desired_velocity = input_dir.normalized() * max_speed
+
+	var next_global_position := global_position + desired_velocity * delta
+	var next_tile := frontlayer.local_to_map(frontlayer.to_local(next_global_position))
+
 	if frontlayer and frontlayer.is_tile_walkable(next_tile):
-		velocity = desired_velocity
+		if is_dashing:
+			velocity = desired_velocity
+		else:
+			velocity = desired_velocity
 	else:
 		velocity = Vector2.ZERO
+		if is_dashing:
+			stop_dash()
 
-	move_and_slide()
-	
 	_update_facing()
+	move_and_slide()
+	if is_dashing:
+		dash_duration_timer -= delta
+
+		if dash_duration_timer <= 0.0:
+			stop_dash()
+		else:
+			dash_timer += delta
+
+			if dash_timer >= AFTERIMG_INTERVAL:
+				dash_timer = 0.0
+				spawn_afterimage()
+	else:
+		dash_timer = 0.0
+
 
 
 func _get_next_walkable_tile(input_dir: Vector2) -> Vector2i:
@@ -73,7 +113,41 @@ func _get_next_walkable_tile(input_dir: Vector2) -> Vector2i:
 
 	return Vector2i(-1, -1)
 
+#Dashing
+func start_dash():
+	if is_dashing:
+		return
 
+	if dash_cooldown_timer > 0.0:
+		return
+
+	if last_direction == Vector2.ZERO:
+		return
+
+	is_dashing = true
+	dash_duration_timer = dash_duration
+	dash_timer = 0.0
+	dash_cooldown_timer = dash_cooldown
+
+	dash_direction = last_direction.normalized()
+
+
+func stop_dash():
+	is_dashing = false
+	
+func spawn_afterimage():
+	var ghost = after_image_scene.instantiate()
+
+	get_parent().add_child(ghost)
+
+	ghost.global_position = global_position
+	ghost.global_rotation = global_rotation
+	ghost.scale = scale
+
+	ghost.setup($PlayerSprite)
+
+
+#Sprite update
 func _update_facing() -> void:
 	if last_direction == Vector2.ZERO:
 		return
